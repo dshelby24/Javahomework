be4c0769c38c62a7d0403016b398172e8fada7ed
domynique <dshelbyv@gmail.com>
2021-04-11 23:54:37 -0400

    push

---
+++ index.js
@@ -0,0 +1,14 @@
+// the first alert 
+// alert ("Hello World"); 
+
+//the second alert
+// alert ("Hello World");
+
+//make the image invisible 
+//move the image  fromtop  to bottom 
+//make the image visible 
+
+
+
+
+
